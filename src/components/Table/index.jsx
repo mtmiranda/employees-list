ca678db0e00e99d0679076b3828dd380ec81ffad
mtmiranda <mattheus_miranda@hotmail.com>
2021-09-16 18:56:20 -0300

    Table component

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styles from './table.module.scss';
+import TableItem from 'components/TableItem';
+
+export const Table = ({ employees }) => {
+    return (
+        <div className={styles.tableContainer}>
+            <div className={styles.tableWrapper}>
+                <table className={styles.tableEmployees}>
+                    <thead>
+                        <tr>
+                            <th>Foto</th>
+                            <th>Nome</th>
+                            <th>Cargo</th>
+                            <th>Data de Admissão</th>
+                            <th>Telefone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map((employee) => (
+                            <TableItem key={employee.id} employee={employee} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Table;
