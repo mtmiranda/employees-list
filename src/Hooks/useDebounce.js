8cec2b7197a9b573e4edcce12490e7b167f40f16
mtmiranda <mattheus_miranda@hotmail.com>
2021-09-16 18:58:05 -0300

    Adding usefull Hooks

---
+++ src/Hooks/useDebounce.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value, delay);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setDebouncedValue(value), delay);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [value, delay]);
+    return debouncedValue;
+};
