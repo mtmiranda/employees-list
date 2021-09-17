a89c61ad8cc0ff7d709a71531fd5cc85bc3f8000
mtmiranda <mattheus_miranda@hotmail.com>
2021-09-17 18:22:21 -0300

    tablePropsMock for tests

---
+++ src/utils/tablePropsMock.js
@@ -0,0 +1,24 @@
+const tablePropsMock = {
+    employees: [
+        {
+            id: 1,
+            name: 'Nome',
+            job: 'job',
+            star: 10,
+            admission_date: '2019-12-02T00:00:00.000Z',
+            phone: '5551234567890',
+            image: 'https://image.jpg',
+        },
+        {
+            id: 2,
+            name: 'Nome',
+            job: 'job',
+            star: 10,
+            admission_date: '2019-12-02T00:00:00.000Z',
+            phone: '5551234567890',
+            image: 'https://image.jpg',
+        },
+    ],
+};
+
+export default tablePropsMock;
